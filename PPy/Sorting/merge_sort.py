b58db71d39d9a0cdc79a16691b0fef72d5e499c0
ashishpydev <ashishpydev@gmail.com>
2017-06-14 07:55:00 +0530

    Update merge_sort.py

--- PPy/Sorting/merge_sort.py
+++ PPy/Sorting/merge_sort.py
@@ -1,32 +1,36 @@
-def merge_sort(A):
-	merge_sort2(A, 0, len(A) - 1)
+def mergeSort(alist):
+    print("Splitting ",alist)
+    if len(alist)>1:
+        mid = len(alist)//2
+        lefthalf = alist[:mid]
+        righthalf = alist[mid:]
 
+        mergeSort(lefthalf)
+        mergeSort(righthalf)
 
-def merge_sort2(A, first, last):
-	if last - first < threshold and first < last:
-		quick_selection(A, first, last)
-	elif first < last:
-		middle = (first + last) // 2
-		merge_sort2(A, first, middle)
-		merge_sort2(A, middle + 1, last)
-		merge(A, first, middle, last)
+        i=0
+        j=0
+        k=0
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                alist[k]=lefthalf[i]
+                i=i+1
+            else:
+                alist[k]=righthalf[j]
+                j=j+1
+            k=k+1
 
+        while i < len(lefthalf):
+            alist[k]=lefthalf[i]
+            i=i+1
+            k=k+1
 
-def merge (A, first, middle, last):
-	L = A[first:middle]
-	R = A[middle:last + 1]
-	L.append(999999999)
-	R.append(999999999)
-	i = j = 0
-	
-	for k in range(first, last + 1):
-		if L[i] <= R[j]:
-			A[k] = L[i]
-			i += 1
-		else:
-			A[k] = R[j]
-			j += 1
-	print(A)
+        while j < len(righthalf):
+            alist[k]=righthalf[j]
+            j=j+1
+            k=k+1
+    print("Merging ",alist)
 
-
-merge_sort([4, 9, 24, 6, 56])
+alist = [54,26,93,17,77,31,44,55,20]
+mergeSort(alist)
+print(alist)
