37b605acec8b91e24c8c662df7f23c74eee7caac
ashishpydev <ashishpydev@gmail.com>
2017-06-14 07:54:19 +0530

    Update BinaryTree.py

--- PPy/BST/BinaryTree.py
+++ PPy/BST/BinaryTree.py
@@ -1,98 +1,136 @@
+# Python program to demonstrate delete operation
+# in binary search tree
+
+# A Binary Tree Node
 class Node:
-	def __init__(self, val):
-		self.value = val
-		self.leftChild = None
-		self.rightChild = None
-	
-	def insert(self, data):
-		if self.value == data:
-			return False
-		elif self.value > data:
-			if self.leftChild:
-				return self.leftChild.insert(data)
-			else:
-				self.leftChild = Node(data)
-				return True
-		else:
-			if self.rightChild:
-				return self.rightChild.insert(data)
-			else:
-				self.rightChild = Node(data)
-				return True
-	
-	def find(self, data):
-		if self.value == data:
-			return True
-		elif self.value > data:
-			if self.leftChild:
-				return self.leftChild.find(data)
-			else:
-				return False
-		else:
-			if self.rightChild:
-				return self.rightChild.find(data)
-			else:
-				return False
-	
-	def preorder(self):
-		if self:
-			print(str(self.value))
-			if self.leftChild:
-				self.leftChild.preorder()
-			if self.rightChild:self.rightChild.preorder()
-	
-	def postorder(self):
-		if self:
-			if self.leftChild:
-				self.leftChild.postorder()
-			if self.rightChild:
-				self.rightChild.postorder()
-			print(str(self.value))
-
-	def inorder(self):
-		if self:
-			print(str(self.value))
-			if self.leftChild:
-				self.leftChild.inorder()
-			print(str(self.value))
-			if self.rightChild:
-				self.rightChild.inorder()
+    # Constructor to create a new node
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
 
 
-class Tree:
-	def __init__(self):
-		self.root = None
-	
-	def insert(self, data):
-		if self.root:
-			return self.root.insert(data)
-		else:
-			self.root = Node(data)
-			return True
-	
-	def find(self, data):
-		if self.root:
-			return self.root.find(data)
-		else:
-			return False
-		
-	def preorder(self):
-		print("PreOrder")
-		self.root.preorder()
-		
-	def postorder(self):
-		print("PostOrder")
-		self.root.postorder()
-		
-	def inorder(self):
-		print("InOrder")
-		self.root.inorder()
-	
+# A utility function to do inorder traversal of BST
+def inorder(root):
+    if root is not None:
+        inorder(root.left)
+        print
+        root.key,
+        inorder(root.right)
 
-bst = Tree()
-bst.insert(10)
-print(bst.insert(15))
-print(bst.insert(16))
-bst.preorder()
-bst.postorder()
-bst.inorder()
+
+# A utility function to insert a new node with given key in BST
+def insert(node, key):
+    # If the tree is empty, return a new node
+    if node is None:
+        return Node(key)
+
+    # Otherwise recur down the tree
+    if key < node.key:
+        node.left = insert(node.left, key)
+    else:
+        node.right = insert(node.right, key)
+
+    # return the (unchanged) node pointer
+    return node
+
+
+# Given a non-empty binary search tree, return the node
+# with minum key value found in that tree. Note that the
+# entire tree does not need to be searched
+def minValueNode(node):
+    current = node
+
+    # loop down to find the leftmost leaf
+    while (current.left is not None):
+        current = current.left
+
+    return current
+
+
+# Given a binary search tree and a key, this function
+# delete the key and returns the new root
+def deleteNode(root, key):
+    # Base Case
+    if root is None:
+        return root
+
+    # If the key to be deleted is similiar than the root's
+    # key then it lies in  left subtree
+    if key < root.key:
+        root.left = deleteNode(root.left, key)
+
+    # If the kye to be delete is greater than the root's key
+    # then it lies in right subtree
+    elif (key > root.key):
+        root.right = deleteNode(root.right, key)
+
+    # If key is same as root's key, then this is the node
+    # to be deleted
+    else:
+
+        # Node with only one child or no child
+        if root.left is None:
+            temp = root.right
+            root = None
+            return temp
+
+        elif root.right is None:
+            temp = root.left
+            root = None
+            return temp
+
+        # Node with two children: Get the inorder successor
+        # (smallest in the right subtree)
+        temp = minValueNode(root.right)
+
+        # Copy the inorder successor's content to this node
+        root.key = temp.key
+
+        # Delete the inorder successor
+        root.right = deleteNode(root.right, temp.key)
+
+    return root
+
+
+# Driver program to test above functions
+""" Let us create following BST
+              50
+           /     \
+          30      70
+         /  \    /  \
+       20   40  60   80 """
+
+root = None
+root = insert(root, 50)
+root = insert(root, 30)
+root = insert(root, 20)
+root = insert(root, 40)
+root = insert(root, 70)
+root = insert(root, 60)
+root = insert(root, 80)
+
+print
+"Inorder traversal of the given tree"
+inorder(root)
+
+print
+"\nDelete 20"
+root = deleteNode(root, 20)
+print
+"Inorder traversal of the modified tree"
+inorder(root)
+
+print
+"\nDelete 30"
+root = deleteNode(root, 30)
+print
+"Inorder traversal of the modified tree"
+inorder(root)
+
+print
+"\nDelete 50"
+root = deleteNode(root, 50)
+print
+"Inorder traversal of the modified tree"
+inorder(root)
